871aaf6a8cf8bb19e7af730034790e91b3db7fdd
Antonios Kotsis <Antonios.Kotsis@teamviewer.com>
2026-06-15 19:11:25 +0300

    add mock data

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { setupMockApi } from "./lib/mock-data";
+
+// Activate in-browser mock API for demo (remove for production)
+setupMockApi();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
